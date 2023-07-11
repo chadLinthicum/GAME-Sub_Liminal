bdf51db422284c0181f75ddb2089fb9bbbb34710
Chad Linthicum <chad.linthicum@saic.com>
2023-07-11 17:14:39 -0400

    Wall collisions

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 180f;
+
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+        newPos.z = 2;
+        transform.position = newPos;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Debug.Log(horizontalInput);
+
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+
+        // Rotate the player object on its Y-axis when moving left or right
+        if (horizontalInput < 0f)
+        {
+            RotatePlayer(270f);
+        }
+        else if (horizontalInput > 0f)
+        {
+            RotatePlayer(90f);
+        }
+    }
+
+    // Rotate the player object towards the specified Y-axis rotation
+    void RotatePlayer(float targetRotationY)
+    {
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            Quaternion targetRotation = Quaternion.Euler(0f, targetRotationY, 0f);
+            playerObject.transform.rotation = Quaternion.RotateTowards(playerObject.transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+{
+    if (collision.gameObject.CompareTag("Wall-L") || collision.gameObject.CompareTag("Wall-R")  || collision.gameObject.CompareTag("Wall-T")  || collision.gameObject.CompareTag("Wall-B"))
+    {
+
+    }
+}
+}
